785eb5ff54337e81771e0c356f5aabea5a479721
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-05-19 01:16:47 +0530

    docs(example): add example

---
+++ example/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <v-github-icon
+      url="https://github.com/vinayakkulkarni/v-image"
+      bg-color="#fff"
+      fill-color="#fa0"
+    />
+  </div>
+</template>
+
+<script>
+  import VGithubIcon from '../src';
+
+  export default {
+    components: {
+      VGithubIcon,
+    },
+  };
+</script>

28db4bf88979b8bdd89dd66057308e8db2f9c149
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2021-09-05 20:21:02 +0530

    docs: update VGithubIcon to `stories`

---
+++ stories/VGithubIcon.stories.js
@@ -0,0 +1,32 @@
+import { VGithubIcon } from '../src';
+
+export default { title: 'GitHub Icon' };
+
+export const withUrlProp = () => ({
+  components: { VGithubIcon },
+  template: '<v-github-icon url="https://vinayakkulkarni.dev" />',
+});
+
+export const withTopLeftPosition = () => ({
+  components: { VGithubIcon },
+  template:
+    '<v-github-icon url="https://vinayakkulkarni.dev" position="top-left" />',
+});
+
+export const withBgColorProp = () => ({
+  components: { VGithubIcon },
+  template:
+    '<v-github-icon url="https://vinayakkulkarni.dev" bg-color="red" />',
+});
+
+export const withFillColorProp = () => ({
+  components: { VGithubIcon },
+  template:
+    '<v-github-icon url="https://vinayakkulkarni.dev" fill-color="red" />',
+});
+
+export const withBgAndFillColorProp = () => ({
+  components: { VGithubIcon },
+  template:
+    '<v-github-icon url="https://vinayakkulkarni.dev" bg-color="rgba(10, 150, 200)" fill-color="rgba(255, 150, 20)" />',
+});
